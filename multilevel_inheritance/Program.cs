8fd0fa59cfdb0dbe3b91b1c29f6725f31c3cb301
Christen Garcia <30610163+christengarcia@users.noreply.github.com>
2018-03-09 23:43:49 +1100

    Initial commit

---
+++ multilevel_inheritance/Program.cs
@@ -0,0 +1,20 @@
+﻿/*
+ * C# Program to Illustrate Multilevel Inheritance with Virtual Methods
+ */
+
+using System;
+
+namespace multilevel_inheritance
+{
+    public class Person
+    {
+        protected string RNO = "44";
+        protected string name = "Ram";
+        public virtual void GetInfo()
+        {
+            Console.WriteLine("Name: {0}", name);
+            Console.WriteLine("RNO: {0}", RNO);
+            Console.WriteLine();
+        }
+    }
+}
